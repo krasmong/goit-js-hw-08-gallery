2c8cdaf62de66e9532427a5869f69246150335d0
Oleksandr Pomalin <nilamopr@gmail.com>
2020-11-08 19:20:29 +0200

    добавляет делегирование, открытие и закрытие модального окна

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 import galleryItems from './data/gallery-items.js';
 
-// console.log("Исходные данные галлереи");
-// console.log(galleryItems);
-
-
 const refs = {
   gallery: document.querySelector('.js-gallery'),
   lightbox: document.querySelector('.js-lightbox'),
@@ -12,12 +8,11 @@ const refs = {
   closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
 };
 
+
+// Создание и рендер разметки по массиву данных 
 const galleryItemsMarkup = createGalleryItems(galleryItems);
 
 refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-
-
-// Создание и рендер разметки по массиву данных 
 
 function createGalleryItems(item) {
   return item
@@ -36,4 +31,89 @@ function createGalleryItems(item) {
     `;
     })
     .join('');
+}
+
+// Реализация делегирования на галерее
+
+refs.gallery.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const curSrcOrigImg = event.target.dataset.source;
+  const curAltOrigImg = event.target.getAttribute('alt');
+
+  onOpenModal(curSrcOrigImg, curAltOrigImg);
+}
+
+// Открытие модального окна по клику на элементе галереe
+
+function onOpenModal(src, alt) {
+  refs.closeModalBtn.addEventListener('click', onCloseModal);
+  refs.overlay.addEventListener('click', onOverlayClick);
+  window.addEventListener('keydown', onEscKeyPress);
+  window.addEventListener('keydown', onNextImgClick);
+
+  refs.lightbox.classList.add('is-open');
+  refs.lightboxImg.src = src;
+  refs.lightboxImg.alt = alt;
+}
+
+
+// Закрытие модального окна по клику на кнопку
+
+function onCloseModal() {
+  refs.closeModalBtn.removeEventListener('click', onCloseModal);
+  refs.overlay.removeEventListener('click', onOverlayClick);
+  window.removeEventListener('keydown', onEscKeyPress);
+  window.removeEventListener('keydown', onNextImgClick);
+
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImg.src = '';
+  refs.lightboxImg.alt = '';
+}
+
+// Закрытие модального окна по клику на overlay
+function onOverlayClick() {
+  onCloseModal();
+}
+
+// Закрытие модального окна по нажатию клавиши ESC
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+
+  if (event.code === ESC_KEY_CODE) {
+    onCloseModal();
+  }
+}
+
+// Пролистывание изображений галереи
+function onNextImgClick(event) {
+  const RIGHT_KEY_CODE = 'ArrowRight';
+  const LEFT_KEY_CODE = 'ArrowLeft';
+
+  let findIndexImg = galleryItems.findIndex(
+    el => el.original === refs.lightboxImg.src,
+  );
+
+  if (event.code === LEFT_KEY_CODE) {
+    if (findIndexImg === 0) {
+      return;
+    }
+    findIndexImg -= 1;
+  }
+
+  if (event.code === RIGHT_KEY_CODE) {
+    if (findIndexImg === galleryItems.length - 1) {
+      return;
+    }
+    findIndexImg += 1;
+  }
+
+  refs.lightboxImg.src = galleryItems[findIndexImg].original;
+  refs.lightboxImg.alt = galleryItems[findIndexImg].description;
 }
